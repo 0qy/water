72d074a8fbcbe52f9dce7c8d3361f43283a7cfdb
0qy <linqianye333@gmail.com>
2020-04-27 17:47:47 +1200

    fixed test according to changes

--- tests/app.test.js
+++ tests/app.test.js
@@ -4,7 +4,7 @@ import { mount } from 'enzyme'
 import App from '../client/components/App'
 
 test('<App />', () => {
-  const expected = 'React development has begun!'
+  const expected = 'Dev'
   const wrapper = mount(<App />)
   expect(wrapper.text()).toMatch(expected)
 })
