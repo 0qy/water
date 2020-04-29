ee44bab2a263c9ec2ecf678d0514b3e60a991ad1
0qy <linqianye333@gmail.com>
2020-04-30 09:36:13 +1200

    React player

--- client/components/WaterMain.jsx
+++ client/components/WaterMain.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import ReactPlayer from 'react-player'
 
 class WaterMain extends React.Component {
   componentDidMount () {
@@ -16,18 +17,23 @@ class WaterMain extends React.Component {
     alert('video or voice failed to load, please refresh page.')
   }
 
+  clickHandler = () => {
+    console.log('clicked')
+  }
+
   render () {
     return (
       <div id="container">
         <div className="main">
           <div className="fullscreen">
-            <iframe id="iframe" src="https://player.vimeo.com/video/412164550?autoplay=1&loop=1&background=1&muted=0"
+            <ReactPlayer id="player" url='https://vimeo.com/412164550' playing/>
+            {/* <iframe id="iframe" src="https://player.vimeo.com/video/412164550?autoplay=1&loop=1&background=1&muted=0"
               frameBorder="0"
               allow="autoplay; fullscreen"
               allowFullScreen
               title="The Return Of Water"
               data-ready="true"
-              onError={this.errorHandler}></iframe>
+              onError={this.errorHandler}></iframe> */}
             <audio id="audio" loop src="monologue.mp3" onError={this.errorHandler}/>
           </div>
         </div>
