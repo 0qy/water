fe0eddc9083d42d30a62a6d34e2ba9f55d37476f
0qy <linqianye333@gmail.com>
2020-04-29 18:04:14 +1200

    added responsive iframe

--- client/components/WaterStart.jsx
+++ client/components/WaterStart.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const WaterStart = (props) => {
   return (
     <div className="start">
-      <p>This site contains flashing images and autoplay audios.</p>
+      <p>This site contains flashing images and autoplay audio.</p>
       <p className="smallText">(Please refresh if voice or video do not start playing.) </p>
       <br></br>
       <button onClick={props.clickHandler}>Enter</button>
